2619461029aefa855589257f42ac4d6a2cc6e034
DevilKo0l <nguyen.tcao96@gmail.com>
2022-05-28 12:47:47 +0200

    update testimonial section

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { NavigationDots, SocialMedia } from "../components";
 
-const AppWrap = (Components, idName, classNames) =>
+const AppWrap = (Component, idName, classNames) =>
   function HOC() {
     return (
       <div id={idName} className={`app__container ${classNames}`}>
         <SocialMedia />
         <div className="app__wrapper app__flex">
-          <Components />
+          <Component />
 
           <div className="copyright">
             <p className="p-text">@2022 NGUYEN CAO</p>
-            <p className="p-text">All right reserved</p>
+            <p className="p-text">All rights reserved</p>
           </div>
         </div>
 
